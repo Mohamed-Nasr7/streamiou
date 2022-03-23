6542ce74fd2565afc85c9d865aeb485d126831b0
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-03-23 22:22:06 +0200

    Import streams components in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
 
 function App() {
   return <div></div>;
