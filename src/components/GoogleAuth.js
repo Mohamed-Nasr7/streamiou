71c2d59b9209731f0b61cac7206828726dcf9341
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-05-26 15:28:39 +0200

    Call the init function of the gapi client and pass to it an object of clientId and scope properties

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,7 +2,13 @@ import React, { useEffect } from 'react';
 
 function GoogleAuth() {
   useEffect(() => {
-    window.gapi.load('client:auth2');
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId:
+          '907558372126-iormelmouneobr1htfqna2mf7pvhnmkh.apps.googleusercontent.com',
+        scope: 'email',
+      });
+    });
   }, []);
 
   return <div></div>;
