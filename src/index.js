c9ad5a0f502a5ebe73593ef624a04b115a557511
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-07-16 18:42:57 +0200

    Import Provider and createStore from redux and react-redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './components/App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
